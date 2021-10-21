e3c919b93bcc2bdb0e37c537c610f0eb7ead8981
Maxim Lavrinenko <max1mlavrinenko@yandex.ru>
2021-10-21 09:51:44 +0300

    aaa

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import styles from "./App.module.scss";
 
 export const App = () => {
     return (
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <BrowserRouter basename="/">
             <div className={styles.container}>
                 <Route path="/" exact component={Join} />
                 <Route path="/room" component={Room} />
