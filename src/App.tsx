2ba2e2bf41fc5a35543671ad3ce2c560202732cf
Maxim Lavrinenko <max1mlavrinenko@yandex.ru>
2021-10-21 21:36:38 +0300

    Fix header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {HashRouter as Router, Route} from "react-router-dom";
 import {Join} from "./components/Join/Join";
 import {Room} from "./components/Room/Room";
 import "normalize.css";
